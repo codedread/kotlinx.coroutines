c05caff509dbe80d58af3f36e68b66fa3575fcfb
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2022-08-11 20:00:56 +0200

    ~initial

---
+++ kotlinx-coroutines-core/jdk8/src/Foo.kt
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2016-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+package kotlinx.coroutines
+
+import java.util.stream.Stream
+
+public fun somePublicFun(): Stream<Int> = Stream.empty()
---
+++ kotlinx-coroutines-core/jdk8/test/FooTest.kt
@@ -0,0 +1,11 @@
+package kotlinx.coroutines
+
+import kotlin.test.*
+
+class FooTest {
+
+    @Test
+    fun foo() {
+
+    }
+}
